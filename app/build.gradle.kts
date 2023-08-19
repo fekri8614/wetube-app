41b18570022bf2a5e7c9b4d6c6a7a34524924a90
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-19 01:54:04 -0700

    added dependencies to `build.gradle.kts (:app)`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    // UI components
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -59,6 +60,31 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // System Ui Controller
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.17.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    // Live data
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
+
+    // Coil - load image
+    implementation("io.coil-kt:coil-compose:2.3.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    // Kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    // Cokoin - DI
+    implementation("dev.burnoo:cokoin:1.0.0")
+    implementation("dev.burnoo:cokoin-android-viewmodel:1.0.0")
+    implementation("dev.burnoo:cokoin-android-navigation:1.0.0")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
