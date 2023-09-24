3a11d882c3727fe2497b62fa7233fb735e1ec578
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-09-24 08:11:54 -0700

    added bottom-bar stuff

--- app/src/main/java/info/fekri8614/wetube/ui/feature/main/MainScreen.kt
+++ app/src/main/java/info/fekri8614/wetube/ui/feature/main/MainScreen.kt
@@ -1,39 +1,54 @@
 package info.fekri8614.wetube.ui.feature.main
 
 import android.content.res.Configuration
+import android.inputmethodservice.Keyboard
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.burnoo.cokoin.navigation.getNavController
 import info.fekri8614.wetube.ui.theme.*
-import info.fekri8614.wetube.ui.theme.Shapes
-import info.fekri8614.wetube.ui.theme.robotoFamily
 import info.fekri8614.wetube.util.MainTopBar
 import info.fekri8614.wetube.util.textLengthStyle
 
@@ -52,6 +67,13 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 personImage = "https://engineering.unl.edu/images/staff/Kayla-Person.jpg",
                 onPersonClicked = { /*TODO: Handle it*/ }
             )
+        },
+        bottomBar = {
+            BottomNavigator {
+                BottomAppBarItem(icon = Icons.Outlined.Home, isChecked = true, onClicked = {})
+                BottomAppBarItem(icon = Icons.Outlined.Home, isChecked = false, onClicked = {})
+                BottomAppBarItem(icon = Icons.Outlined.Home, isChecked = false, onClicked = {})
+            }
         },
         content = { paddingValues ->
             Column(
@@ -117,4 +139,52 @@ fun AdsBar(
     }
 }
 
+// --------------------------------------------------
 
+@Composable
+fun BottomNavigator(
+    modifier: Modifier = Modifier,
+    containerColor: Color = BottomAppBarDefaults.containerColor,
+    contentColor: Color = contentColorFor(containerColor),
+    tonalElevation: Dp = BottomAppBarDefaults.ContainerElevation,
+    contentPadding: PaddingValues = BottomAppBarDefaults.ContentPadding,
+    windowInsets: WindowInsets = BottomAppBarDefaults.windowInsets,
+    content: @Composable RowScope.() -> Unit
+) {
+    Surface(
+        color = containerColor,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        modifier = modifier
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(windowInsets)
+                .height(80.dp)
+                .padding(contentPadding),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            content = content
+        )
+    }
+}
+
+@Composable
+fun BottomAppBarItem(icon: ImageVector, isChecked: Boolean, onClicked: (Boolean) -> Unit) {
+    IconButton(onClick = { onClicked(false) }) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(imageVector = icon, contentDescription = null)
+            if (isChecked) {
+                Spacer(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .size(width = 10.dp, height = 5.dp)
+                        .clip(RoundedCornerShape(60))
+                )
+            }
+        }
+    }
+}
